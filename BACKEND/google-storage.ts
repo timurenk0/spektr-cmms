28853d2228e2249cede1ea924f6c7faf34dec035
timurenk0 <zheltenkovtimur@gmail.com>
2026-02-20 10:43:30 +0100

    Changed document upload folder name to docs/ | Added '0' padding to month date folder name

--- BACKEND/google-storage.ts
+++ BACKEND/google-storage.ts
@@ -19,13 +19,18 @@ class GStorage {
         return [];
     }
 
+    /**
+     * Uploads the desirable document to Google Storage. Automatically creates folders with date of upload for easier navigation and filetering.
+     * @param doc document file to upload to the Google Storage
+     * @returns public URL to the stored document
+     */
     async uploadDocument(doc: File): Promise<string> {
         const buffer = Buffer.from(await doc.arrayBuffer());
         
         // Generate document name using upload date
         const now = new Date();
-        const date = `${now.getFullYear()}-${now.getMonth()}`;
-        const filePath = `uploads/${date}/${doc.name}`;
+        const date = `${now.getFullYear()}-${now.getMonth().toString().padStart(2, "0")}`;
+        const filePath = `docs/${date}/${doc.name}`;
 
         const gcsFile = bucket.file(filePath);
 
@@ -37,6 +42,20 @@ class GStorage {
         // Uploaded file public URL
         const returnURL = publicURL+`/${filePath}`;
         return returnURL;
+    }
+
+    async getPhotos(): Promise<string[]> {
+        return [];
+    }
+
+   
+    /**
+     * Uploads the desirable image to Google Storage. Automatically creates folders with date of upload for easier navigation and filetering.
+     * @param img image file to upload to the Google Storage
+     * @returns public URL to the stored image
+     */
+    async uploadPhoto(img: File): Promise<string> {
+        return "";
     }
 
 }
