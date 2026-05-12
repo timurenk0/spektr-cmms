db4d2cead9f0c36183921761e42f32ff94c4f7c6
timurenk0 <zheltenkovtimur@gmail.com>
2026-05-12 20:08:57 +0200

    Added generic error handler buildError and custom error handler buildCustomError

--- BACKEND/Utils/errorBuilder.ts
+++ BACKEND/Utils/errorBuilder.ts
@@ -1,4 +1,7 @@
+import { DatabaseError } from "@neondatabase/serverless"
+import { DrizzleQueryError } from "drizzle-orm"
 import { NextResponse } from "next/server"
+import { ZodError } from "zod"
 
 
 type CustomError = {
@@ -17,6 +20,13 @@ type CustomApiError = {
     status: number
 }
 
+export const ERROR_CODES = {
+    UNKNOWN_ERROR: "UNKNOWN_ERROR",
+    SERVER_ERROR: "SERVER_ERROR",
+    VALIDATION_ERROR: "VALIDATION_ERROR",
+    DUPLICATION_ERROR: "DUPLICATION_ERROR"
+}
+
 export class ApiError extends Error {
     code: string;
     status: number;
@@ -33,7 +43,83 @@ export class ApiError extends Error {
     }
 }
 
-export default function buildError({
+/**
+ * Checks generic errors like user authentication/authorization, Zod and Database inconsistencies errors
+ * @param error Any error caught by try-catch block
+ * @returns NextResponse with success flag and custom error
+ */
+export default function buildError(error: unknown) {
+    if (error instanceof ApiError) return buildCustomError(error);
+
+    if (error instanceof ZodError) {
+        console.error(error);
+
+        const firstError = error.issues[0];
+        const field = firstError.path.join(".");
+
+        switch (field) {
+            case "tenantId":
+                return buildCustomError({
+                    code: "TENANT_ERROR",
+                    field,
+                    message: "Current tenant is not linked/activated yet.",
+                    suggestion: "Please contact IT administrator.",
+                    status: 403
+                })
+            default:
+                return buildCustomError({
+                    code: ERROR_CODES.VALIDATION_ERROR,
+                    field,
+                    message: firstError.message,
+                    suggestion: "Double-check the submitted form fields.",
+                    status: 400
+                });
+        }
+    }
+
+    if (error instanceof DrizzleQueryError) {
+        console.error(error);
+        if (error instanceof DatabaseError) {
+            console.error(error);
+
+            return buildCustomError({
+                code: ERROR_CODES.SERVER_ERROR,
+                message: "Something went wrong while executing your query.",
+                suggestion: "Please try again later.",
+                status: 500
+            });
+        }
+
+        console.error(error);
+        return buildCustomError({
+            code: ERROR_CODES.UNKNOWN_ERROR,
+            message: "Unexpected server error",
+            suggestion: "Please try again later",
+            status: 500
+        });
+    }
+}
+
+/**
+ * Builds errors with custom parameters based on the default custom error pattern
+ * @param param0 Custom error parameters
+ * 
+ * {
+ * 
+ *  code: string,
+ * 
+ *  message: string,
+ * 
+ *  field?: string,
+ * 
+ *  suggestion?: string,
+ * 
+ *  status?: string (default = 500)
+ * 
+ * }
+ * @returns Custom error
+ */
+export function buildCustomError({
     code,
     message,
     field,
