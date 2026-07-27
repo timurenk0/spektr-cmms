c268c86cf6430cb81d76da3bf4ea219a3a45e482
timurenk0 <zheltenkovtimur@gmail.com>
2026-07-27 15:08:53 +0200

    Changed interval-based maintenance to interval-based inspection

--- COMPONENTS/Maintenance/AddMaintenanceForm.tsx
+++ COMPONENTS/Maintenance/AddMaintenanceForm.tsx
@@ -450,8 +450,8 @@ const AddMaintenanceForm = ({
            {requirements !== "maintenance" && (
             <>
               <div className="border p-4 rounded-md bg-pink-50 col-span-2">
-              <h3 className="font-medium text-lg">Interval-based Maintenance 1</h3>
-              <p className="font-medium text-xs mb-3 italic">*Maintenance independent of equipment working hours</p>
+              <h3 className="font-medium text-lg">Interval-based Inspection 1</h3>
+              <p className="font-medium text-xs mb-3 italic">*Inspection independent of equipment working hours</p>
               <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
                 <Controller
                   name="levelIMonths1"
@@ -503,8 +503,8 @@ const AddMaintenanceForm = ({
               </div>
             </div>
             <div className="border p-4 rounded-md bg-pink-50 col-span-2">
-              <h3 className="font-medium text-lg">Interval-based Maintenance 2</h3>
-              <p className="font-medium text-xs mb-3 italic">*Maintenance independent of equipment working hours</p>
+              <h3 className="font-medium text-lg">Interval-based Inspection 2</h3>
+              <p className="font-medium text-xs mb-3 italic">*Inspection independent of equipment working hours</p>
               <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
                 <Controller
                   name="levelIMonths2"
